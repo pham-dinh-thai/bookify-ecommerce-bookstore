04eb7dc2b0051e8267255aa5d74fcbb62eec7ec0
Th41 <phamdinhthai2005@gmail.com>
2026-04-30 00:31:02 +0700

    add logo

--- frontend/src/app/(shop)/components/navigation-bar/partials/nav-logo.tsx
+++ frontend/src/app/(shop)/components/navigation-bar/partials/nav-logo.tsx
@@ -1,4 +1,4 @@
-import { BookOpen } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 type NavLogoProps = {
@@ -9,7 +9,7 @@ export default function NavLogo({ appName }: NavLogoProps) {
   return (
     <Link href="/">
       <span className="flex items-center gap-1 text-sm font-bold tracking-tight shrink-0 text-[#2d6a4f]">
-        <BookOpen /> {appName}
+        <Image src="/icon.png" alt="Logo" width={32} height={32} /> {appName}
       </span>
     </Link>
   );
