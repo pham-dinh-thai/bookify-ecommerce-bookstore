360af365b00a9a2b0aa66951e5635d593ec1091c
Th41 <phamdinhthai2005@gmail.com>
2026-05-14 16:14:23 +0700

    implement file deletion functionality and related request validation

--- backend/src/modules/file-storage/file-storage.module.ts
+++ backend/src/modules/file-storage/file-storage.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './presentation/files/files.controller';
 import { UuidModule } from '../../shared/uuid/uuid.module';
-import { STORAGE_PROVIDER } from './application/file-use-cases/ports/storage-provider.interface';
+import { STORAGE_PROVIDER } from './domain/file-aggregate/storage/storage-provider.interface';
 import { LocalStorageProvider } from './infrastructure/storage/local-storage.provider';
 import { UploadFileUseCase } from './application/file-use-cases/upload-file/upload-file.use-case';
+import { DeleteFileUseCase } from './application/file-use-cases/delete-file/delete-file.use-case';
 
 @Module({
   controllers: [FilesController],
   imports: [UuidModule],
   providers: [
     UploadFileUseCase,
+    DeleteFileUseCase,
     {
       provide: STORAGE_PROVIDER,
       useClass: LocalStorageProvider,
--- backend/src/modules/file-storage/infrastructure/storage/local-storage.provider.ts
+++ backend/src/modules/file-storage/infrastructure/storage/local-storage.provider.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
-import { IStorageProvider } from '../../application/file-use-cases/ports/storage-provider.interface';
+import { IStorageProvider } from '../../domain/file-aggregate/storage/storage-provider.interface';
 
 export class LocalStorageProvider implements IStorageProvider {
   private readonly uploadDir = '/storage/uploads';
@@ -11,12 +11,15 @@ export class LocalStorageProvider implements IStorageProvider {
     mimetype: string,
   ): Promise<string> {
     await fs.mkdir(this.uploadDir, { recursive: true });
+
     await fs.writeFile(path.join(this.uploadDir, filename), buffer);
+
     return `/uploads/${filename}`;
   }
 
   public async delete(url: string): Promise<void> {
     const filename = path.basename(url);
+
     await fs.unlink(path.join(this.uploadDir, filename));
   }
 }
