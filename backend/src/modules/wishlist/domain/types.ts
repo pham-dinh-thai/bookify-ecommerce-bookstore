f17646e24c99eaedaed1d414ee16baa85758e56f
zafkiel <phamdinhthai2005@gmail.com>
2026-08-02 22:45:13 +0700

    feat(wishlist): implement wishlist aggregate and related entities and types

---
+++ backend/src/modules/wishlist/domain/types.ts
@@ -0,0 +1,10 @@
+export type CreateWishlistProps = {
+  id: string;
+  userId: string;
+};
+
+export type FromPersistentWishlistProps = {
+  id: string;
+  userId: string;
+  items: any;
+};
---
+++ backend/src/modules/wishlist/domain/wishlist.aggregate.ts
@@ -0,0 +1,30 @@
+import { WishlistItem } from './entities/wishlist-item.entity';
+import { CreateWishlistProps, FromPersistentWishlistProps } from './types';
+
+export class Wishlist {
+  public constructor(
+    private readonly id: string,
+    private readonly userId: string,
+    private items: WishlistItem[],
+  ) {}
+
+  public static create(props: CreateWishlistProps): Wishlist {
+    return new Wishlist(props.id, props.userId, []);
+  }
+
+  public static fromPersistent(props: FromPersistentWishlistProps): Wishlist {
+    return new Wishlist(props.id, props.userId, props.items);
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public getUserId(): string {
+    return this.userId;
+  }
+
+  public getItems(): WishlistItem[] {
+    return [...this.items];
+  }
+}
