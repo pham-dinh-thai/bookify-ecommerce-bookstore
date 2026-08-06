095877bccfc496abe71a232f1f262bdf7a377e7d
zafkiel <phamdinhthai2005@gmail.com>
2026-08-06 15:04:20 +0700

    feat(wishlist): findWishlistUsersByBookId

---
+++ backend/src/modules/wishlist/domain/read-models/wishlist-user.read-model.ts
@@ -0,0 +1,8 @@
+export class WishlistUserReadModel {
+  public constructor(
+    public readonly userId: string,
+    public readonly email: string,
+    public readonly firstName: string,
+    public readonly lastName: string,
+  ) {}
+}
--- backend/src/modules/wishlist/infrastructure/repositories/typeorm-wishlists-query.repository.ts
+++ backend/src/modules/wishlist/infrastructure/repositories/typeorm-wishlists-query.repository.ts
@@ -3,7 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { IWishlistsQueryRepository } from '../../domain/repositories/wishlists-query.repository.interface';
 import { WishlistReadModel } from '../../domain/read-models/wishlist.read-model';
+import { WishlistUserReadModel } from '../../domain/read-models/wishlist-user.read-model';
 import { WishlistTypeOrm } from '../entities/wishlist.entity';
+import { WishlistItemTypeOrm } from '../entities/wishlist-item.entity';
 import { WishlistsMapper } from '../mappers/wishlists.mapper';
 
 @Injectable()
@@ -11,6 +13,9 @@ export class TypeOrmWishlistsQueryRepository implements IWishlistsQueryRepositor
   public constructor(
     @InjectRepository(WishlistTypeOrm)
     private readonly repository: Repository<WishlistTypeOrm>,
+
+    @InjectRepository(WishlistItemTypeOrm)
+    private readonly wishlistItemRepository: Repository<WishlistItemTypeOrm>,
   ) {}
 
   public async findUserWishlist(
@@ -38,5 +43,39 @@ export class TypeOrmWishlistsQueryRepository implements IWishlistsQueryRepositor
     }
 
     return WishlistsMapper.toReadModel(wishlistTypeOrm);
+  }
+
+  public async findWishlistUsersByBookId(
+    bookId: string,
+  ): Promise<WishlistUserReadModel[]> {
+    const wishlistItems = await this.wishlistItemRepository.find({
+      relations: {
+        wishlist: {
+          user: true,
+        },
+      },
+      where: { itemId: bookId },
+    });
+
+    const usersByUserId = new Map<string, WishlistUserReadModel>();
+
+    for (const item of wishlistItems) {
+      const user = item.wishlist.user;
+      if (usersByUserId.has(user.id)) {
+        continue;
+      }
+
+      usersByUserId.set(
+        user.id,
+        new WishlistUserReadModel(
+          user.id,
+          user.email,
+          user.firstName,
+          user.lastName,
+        ),
+      );
+    }
+
+    return [...usersByUserId.values()];
   }
 }
